d0c14d4d8918731db931af9a05397d23a99f7ef5
yanghuanyu <yangyh94@163.com>
2019-08-06 13:54:41 +0800

    m

---
+++ eureka_server/src/test/java/com/grand/eureka/EurekaApplicationTests.java
@@ -0,0 +1,18 @@
+/*
+package com.grand.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTe«st;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+*/
